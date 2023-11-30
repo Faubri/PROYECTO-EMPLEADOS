cc3f2a37f239a8339ca6630dff5ee219fa465ecb
javier mora <morajavier393@gmail.com>
2023-11-30 12:23:43 -0500

    se crea y configura la parte backend del proyecto junto con la conexion a la base de datos de mongodb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { HttpClientModule} from "@angular/common/http";
 import { TableModule } from 'primeng/table';
 import {ConfirmationService, MessageService} from "primeng/api";
 import { AppComponent } from './app.component';
-import { UsuarioComponent } from './module/usuario/usuario.component';
 import {FormsModule} from "@angular/forms";
 import {CardModule} from "primeng/card";
 import {ButtonModule} from "primeng/button";
 import {DockModule} from "primeng/dock";
-import { ProductoComponent } from './module/producto/producto.component';
 import {DialogModule} from 'primeng/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent,
-    ProductoComponent
   ],
   imports: [
     BrowserModule,
